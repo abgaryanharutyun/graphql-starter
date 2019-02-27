6b85e4fa98b717d0062845ceedd4f9be5828060b
Harut Abgaryan <harutyun.abgaryan@gmail.com>
2019-02-28 00:28:36 +0400

    added relation ships

--- src/index.js
+++ src/index.js
@@ -1,54 +1,133 @@
-import { GraphQLServer } from 'graphql-yoga';
-
+import {
+  GraphQLServer
+} from 'graphql-yoga';
 
 // Scalar types = String, Boolean, Inr, Float, ID
+const users = [{
+    id: '1',
+    name: 'Harut',
+    email: 'harut@gmail.com',
+    age: 25
+  },
+  {
+    id: '2',
+    name: 'Harut1',
+    email: 'harut1@gmail.com',
+    age: 25
+  }
+];
 
+const posts = [{
+    id: '1',
+    title: 'Harut',
+    body: 'Harut',
+    published: true,
+    author: '1',
+  },
+  {
+    id: '2',
+    title: 'Harutqqqq',
+    body: 'Harutqqqqq',
+    published: true,
+    author: '2',
+  }
+];
+
+const comments = [{
+    id: '1',
+    text: 'first comment',
+    author: '1',
+    postId: '1',
+  },
+  {
+    id: '5',
+    text: 'first commentsss',
+    author: '1',
+    postId: '1',
+  },
+  {
+    id: '2',
+    text: 'first comment',
+    author: '2',
+    postId: '2',
+  }
+]
 const typeDefs = `
   type Query {
-    greeting(name: String): String!
-    id: ID!
-    name: String!
-    age: Int!
-    employed: Boolean!
-    gpa: Float
+    users(query: String): [User]!
+    posts(query: String): [Post]!
+    comments: [Comment!]!
     me: User!
+    post: Post!
   }
 
   type User {
     id: ID!
     name: String!
     email: String!
+    posts: [Post!]!
+    comments: [Comment!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
   }
 `
 
 const Resolve = {
   Query: {
-    greeting(parent, {name}, ctx, info) {
-      return `Name is ${name}`
-    },
-    id() {
-      return 'abc123'
-    },
-    name() {
-      return "Hart"
-    },
-    age() {
-      return 12
-    },
-    employed() {
-      return true
-    },
-    gpa() {
-      return 3.15
-    },
     me() {
       return {
         id: '123',
         name: 'Harut',
         email: 'harut@tpl.com'
       }
+    },
+    users(parent, args, ctx, info) {
+        return users
+    },
+    posts() {
+      return posts
+    },
+    comments() {
+      return comments
     }
   },
+  
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.filter(user => user.id === parent.author)
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => comment.postId === parent.id)
+    }
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find(user => user.id === parent.author)
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => post.id === parent.id)
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id
+      })
+    }
+  }
 }
 
 const server = new GraphQLServer({
